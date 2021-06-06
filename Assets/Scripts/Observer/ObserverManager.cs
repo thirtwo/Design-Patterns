857e0779fb40246345b8ee834d51fe3b5c67fb81
thirtwo <65511956+thirtwo@users.noreply.github.com>
2021-06-06 13:01:20 +0300

    Observer Pattern with C# Actions added

---
+++ Assets/Scripts/Observer/ObserverManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+public class ObserverManager : MonoBehaviour
+{
+    public static event Action OnClickButton;
+
+
+    public void ClickedButton()
+    {
+        OnClickButton?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Observer/RandomColorChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class RandomColorChanger : MonoBehaviour
+{
+  
+    private Material material;
+    
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        ObserverManager.OnClickButton += ChangeRandomColor;
+    }
+
+
+    void ChangeRandomColor()
+    {
+        material.color = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+    }
+
+    private void OnDestroy()
+    {
+        ObserverManager.OnClickButton -= ChangeRandomColor;
+    }
+}
